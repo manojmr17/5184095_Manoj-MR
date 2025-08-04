b583b872077076bf3e090f1f2b863a827d12d500
manojmr17 <manojmr1703@gmail.com>
2025-08-04 20:01:59 +0530

    switch

--- Test.c
+++ Test.c
@@ -1,42 +1,30 @@
-
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main()
+{
     int day;
-    
+    do{ printf("Enter the number b/w 1 to7:");
+        scanf("%d",&day);
+        switch(day){
+        case 1:printf("Monday \n");
+        break;
+        case 2:printf("Tuesday \n");
+        break;
+        case 3:printf("Wednesday\n");
+        break;
+        case 4:printf("Thursday\n");
+        break;
+        case 5:printf("Friday\n");
+        break;
+        case 6:printf("Saturday\n");
+        break;
+        case 7:printf("Sunday\n");
+        break;
+        default:printf("invalid \n");
 
-    while(day>7){
-    printf("Enter a number (1 to 7) : ");
-    scanf("%d", &day);
+        }
 
-    switch(day) {
-        case 1:
-            printf("Sunday\n");
-            break;
-        case 2:
-            printf("Monday\n");
-            break;
-        case 3:
-            printf("Tuesday\n");
-            break;
-        case 4:
-            printf("Wednesday\n");
-            break;
-        case 5:
-            printf("Thursday\n");
-            break;
-        case 6:
-            printf("Friday\n");
-            break;
-        case 7:
-            printf("Saturday\n");
-            break;
-        default:
-            printf("Invalid input Please enter a number between 1 and 7\n");
-            
-    
-    }
-    }
-
+    }while(day<0 || day>=7);
     return 0;
 }
